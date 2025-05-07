afd5b2269d48342aee1611080eb14322ac224161
Thirza Swijnenburg <Thirza.swijnenburg@gmail.com>
2025-05-08 00:04:27 +0200

    v2.1 Styled a bit

--- frontend/src/app/components/task-list/task-list.component.ts
+++ frontend/src/app/components/task-list/task-list.component.ts
@@ -8,7 +8,7 @@ import { TaskEditDialogComponent } from '../task-edit-dialog/task-edit-dialog.co
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
   tasks: any[] = [];
